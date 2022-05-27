f406f233d42ce5da2ed876d17dab3fe0c880ffcb
dmitry <dmitrsafonov@gmail.com>
2022-05-27 14:32:05 +0300

    решение разднления чытерехзначного числа

--- prog from stepic/numbers 2 step.py
+++ prog from stepic/numbers 2 step.py
@@ -1,5 +1,8 @@
-num = int(input())
-if num%2==0:
-    print("Четное число")  
-else:
-     print("Нечетное число") 
+num = int(input()) # сумма первой и последней цифр равна разности второй и третьей цифр.
+a = num//1000
+num1 = a//100
+num2 = a//10 % 10
+num3 = a % 10
+b = num % 1000
+num4 = b //100
+print(num1,num2,num3,num4)
