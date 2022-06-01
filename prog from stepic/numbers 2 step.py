f9cc960ef65735994375f5ac9a756b973f553b7b
dmitry <dmitrsafonov@gmail.com>
2022-06-01 15:59:09 +0300

    роскомнадзор

--- prog from stepic/numbers 2 step.py
+++ prog from stepic/numbers 2 step.py
@@ -1,8 +1,5 @@
-num = int(input()) # сумма первой и последней цифр равна разности второй и третьей цифр.
-a = num//1000
-num1 = a//100
-num2 = a//10 % 10
-num3 = a % 10
-b = num % 1000
-num4 = b //100
-print(num1,num2,num3,num4)
+num = int(input())
+if num >=18:
+    print("доступ разрешен")
+else:
+    print("доступ запрещен")
